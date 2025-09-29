4bcfe6d4ff752f362a3f28d21ce41c100e522573
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-29 15:45:16 +0500

    Create build tree function, and class tree with removedubplicates as utility function

--- javascript/cs-section/binary-tree/BST.js
+++ javascript/cs-section/binary-tree/BST.js
@@ -1,7 +1,39 @@
 class Node {
   constructor(value) {
-    this.value = value;
+    this.data = value;
     this.left = null;
     this.right = null;
   }
 }
+
+class Tree {
+  constructor(...args) {
+    args = removeDuplicates(args).sort((a, b) => a - b);
+    this.root = buildTree(args)
+  }
+}
+// Function that removes duplicates
+function removeDuplicates(args) {
+  for (let i = 0; i < args.length; i++) {
+    for (let j =i + 1; j < args.length - 1; j++) {
+      if (args[i] === args[j]) {
+        args.splice(j, 1)
+      }
+    }
+  }
+  return args;
+}
+
+function buildTree(args) {
+  if (args.length === 0) {
+    return null;
+  }
+  const rootIndex = Math.floor(args.length / 2);
+  const root = new Node(args[rootIndex]);
+  const left = args.slice(0, rootIndex);
+  const right = args.slice(rootIndex + 1);
+  root.left = buildTree(left);
+  root.right = buildTree(right);
+  return root;
+}
+const tree = new Tree(1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324);
