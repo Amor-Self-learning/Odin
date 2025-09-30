04c2bbb2785bdb8c5f09f06fc2c9445c90eac78f
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-30 17:33:00 +0500

    Create lever order for each for BST ..

--- javascript/cs-section/binary-tree/BST.js
+++ javascript/cs-section/binary-tree/BST.js
@@ -67,7 +67,26 @@ class Tree {
     }
     return root;
   }
+  
+  levelOrderForEach(callback, root) {
+    if (!callback || typeof callback !== 'function') {
+      throw new TypeError(`Require a callback here you provided ${callback}`)
+    }
+    let queue = [root]
+    while(queue.length > 0) {
+      const node = queue.shift();
 
+      callback(node);
+
+      if(node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+    
+  }
 }
 function getSuccessor(curr) {
     curr = curr.right;
@@ -102,3 +121,29 @@ function buildTree(args) {
   return root;
 }
 const tree = new Tree(1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324);
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+function increaseByOne(arg) {
+  arg.value += 1;
+}
+prettyPrint(tree.root)
+
+tree.insert(13, tree.root);
+prettyPrint(tree.root)
+console.log(tree.findValue(13, tree.root))
+tree.deleteItem(13, tree.root);
+prettyPrint(tree.root)
+tree.levelOrderForEach(increaseByOne, tree.root)
+prettyPrint(tree.root)
